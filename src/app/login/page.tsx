507b3c4ceb32d78af005b51b9a8c0d7b1ea0fd1b
Aditya Nalawade <157732130+Adiiiicodes@users.noreply.github.com>
2025-06-28 20:06:52 +0530

    Fix searchParams invariant error by removing useSearchParams

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,8 @@
 // src/app/login/page.tsx
 'use client';
 
-import { Suspense } from 'react';
 import LoginPage from '@/components/LoginPage';
 
-function LoginPageContent() {
-  return <LoginPage />;
-}
-
 export default function Login() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <LoginPageContent />
-    </Suspense>
-  );
+  return <LoginPage />;
 }
